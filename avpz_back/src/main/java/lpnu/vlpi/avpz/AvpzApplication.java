0d29c6a185fe4e49fedaca1c18fd8b47a2d2d48a
markusha <marko.pz.2-18@gmail.com>
2021-11-07 16:48:45 +0200

    Init commit

---
+++ avpz_back/src/main/java/lpnu/vlpi/avpz/AvpzApplication.java
@@ -0,0 +1,13 @@
+package lpnu.vlpi.avpz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvpzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvpzApplication.class, args);
+    }
+
+}
---
+++ avpz_back/src/test/java/lpnu/vlpi/avpz/AvpzApplicationTests.java
@@ -0,0 +1,13 @@
+package lpnu.vlpi.avpz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvpzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
