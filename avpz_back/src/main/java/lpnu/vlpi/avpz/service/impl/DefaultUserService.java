d34d33d2335b9c63961e9c5d5d0ad83d8cf67a89
markusha <marko.pz.2-18@gmail.com>
2021-11-24 23:28:47 +0200

    Special for OSTAP

--- avpz_back/src/main/java/lpnu/vlpi/avpz/service/impl/DefaultUserService.java
+++ avpz_back/src/main/java/lpnu/vlpi/avpz/service/impl/DefaultUserService.java
@@ -13,10 +13,12 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class DefaultUserService implements UserService {
 
     private UserRepository userRepository;
