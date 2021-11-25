91eb9ed8dde5fc2ec93f6230cafd3b1db0777161
markusha <marko.pz.2-18@gmail.com>
2021-11-25 22:23:33 +0200

    Special for OSTAP

--- avpz_back/src/main/java/lpnu/vlpi/avpz/service/populator/UserUpdatePopulator.java
+++ avpz_back/src/main/java/lpnu/vlpi/avpz/service/populator/UserUpdatePopulator.java
@@ -1,6 +1,7 @@
 package lpnu.vlpi.avpz.service.populator;
 
 import lpnu.vlpi.avpz.dto.user.UserUpdateDTO;
+import lpnu.vlpi.avpz.helper.EncryptionHelper;
 import lpnu.vlpi.avpz.model.UserModel;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +11,12 @@ import java.text.SimpleDateFormat;
 @Component
 public class UserUpdatePopulator implements Populator<UserUpdateDTO, UserModel> {
     private SimpleDateFormat bdFormat = new SimpleDateFormat("yyyy-MM-dd");
+    private EncryptionHelper encryptionHelper;
+
+    public UserUpdatePopulator(EncryptionHelper encryptionHelper) {
+        this.encryptionHelper = encryptionHelper;
+    }
+
     @Override
     public void populate(UserUpdateDTO userUpdateDTO, UserModel userModel) throws ParseException {
         userModel.setName(userUpdateDTO.getName());
@@ -18,7 +25,7 @@ public class UserUpdatePopulator implements Populator<UserUpdateDTO, UserModel> 
         userModel.setBirthDate(bdFormat.parse(userUpdateDTO.getBirthday()));
         String password = userUpdateDTO.getPassword();
         if(password != null) {
-            userModel.setPassword(password);
+            userModel.setPassword(encryptionHelper.encrypt(password));
         }
     }
 }
